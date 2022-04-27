6eeffe1b3ed6a683b8a9d8cf27796aa92b06a4df
blast <rodfval.fv@gmail.com>
2022-04-27 16:06:10 +0000

    aaa

--- commands/youtube.js
+++ commands/youtube.js
@@ -5,26 +5,26 @@ const fs = require('fs');
 const config = require("../config.json");
 const player = createAudioPlayer();
 const fetch = require('node-fetch');
+const http = require("http");
 
 const path = config.CACHE_DIR;
 const api=config.API_URL;
-const text="&text=";
+const hostname=config.API_HOSTNAME;
 const path_audio="/chatbot_music/"
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('youtube')
         .setDescription('Il pezzente riproduce audio da un video di youtube')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('search')
-                .setDescription('Ricerca su youtube')
-                .addStringOption(option => option.setName('input').setDescription('Il testo da cercare').setRequired(true)))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('link')
-                .setDescription('Riproduci un link')
-                .addStringOption(option => option.setName('input').setDescription('Il link da riprodurre').setRequired(true))),
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('Hai un link o vuoi cercare qualcosa?')
+                .setRequired(true)
+                //.addChoice('ricerca','ricerca')
+                .addChoice('link','link')
+                )
+            //.addStringOption(option => option.setName('video').setDescription('link o ricerca').setRequired(true)
+            .addStringOption(option => option.setName('video').setDescription('link').setRequired(true)),
     async execute(interaction) {
         if (interaction.member.voice === null 
             || interaction.member.voice === undefined 
@@ -59,45 +59,84 @@ module.exports = {
             }
             interaction.deferReply({ ephemeral: true});
 
-            const link = interaction.options.getString('input');
+            const input = interaction.options.getString('input');            
+            const video = interaction.options.getString('video');
+            
+            if( input === 'link' ) {
 
-            var params = api+path_audio+'youtube/get?url='+encodeURIComponent(link);
+                if ( !video.startsWith('http')) {                    
+                    interaction.editReply({ content: 'Devi inserire un url di youtube se vuoi riprodurre da un link', ephemeral: true });   
+                } else {
 
-            fetch(
-                params,
-                {
-                    method: 'GET',
-                    headers: { 'Accept': '*/*' }
+                    var params = api+path_audio+'youtube/get?url='+encodeURIComponent(video);
+
+                    fetch(
+                        params,
+                        {
+                            method: 'GET',
+                            headers: { 'Accept': '*/*' }
+                        }
+                    ).then(res => {
+                        new Promise((resolve, reject) => {
+                            var file = Math.random().toString(36).slice(2)+".mp3";
+                            //var file = "temp.wav";
+                            var outFile = path+"/"+file;
+                            const dest = fs.createWriteStream(outFile);
+                            res.body.pipe(dest);
+                            res.body.on('end', () => resolve());
+                            dest.on('error', reject);
+
+                            dest.on('finish', function(){      
+                                connection.subscribe(player);                      
+                                const resource = createAudioResource(outFile, {
+                                    inputType: StreamType.Arbitrary,
+                                });
+                                player.play(resource); 
+                                const row = new MessageActionRow()
+                                .addComponents(
+                                    new MessageButton()
+                                        .setCustomId('stop')
+                                        .setLabel('Stop')
+                                        .setStyle('PRIMARY'),
+                                );
+                                interaction.editReply({ content: 'Il pezzente sta riproducendo', ephemeral: true, components: [row] });           
+                            });
+                        })
+                    }).catch(function(error) {
+                        console.log(error);
+                    }); 
                 }
-            ).then(res => {
-                new Promise((resolve, reject) => {
-                    var file = Math.random().toString(36).slice(2)+".mp3";
-                    //var file = "temp.wav";
-                    var outFile = path+"/"+file;
-                    const dest = fs.createWriteStream(outFile);
-                    res.body.pipe(dest);
-                    res.body.on('end', () => resolve());
-                    dest.on('error', reject);
+            } else if( input === 'ricerca' ) {
 
-                    dest.on('finish', function(){      
-                        connection.subscribe(player);                      
-                        const resource = createAudioResource(outFile, {
-                            inputType: StreamType.Arbitrary,
+                if ( video.startsWith('http')) {                    
+                    interaction.editReply({ content: 'Devi selezionare "link" se vuoi riprodurre un url di youtube', ephemeral: true });   
+                } else {
+
+                    const options = {
+                        "method": "GET",
+                        "hostname": hostname,
+                        "port": 5080,
+                        "path": path_audio+'youtube/search?text='+encodeURIComponent(video)
+                    }
+                    const req = http.request(options, function(res) {
+
+                        var chunks = [];
+                    
+                        res.on("data", function (chunk) {
+                            chunks.push(chunk);
                         });
-                        player.play(resource); 
-                        const row = new MessageActionRow()
-                        .addComponents(
-                            new MessageButton()
-                                .setCustomId('stop')
-                                .setLabel('Stop')
-                                .setStyle('PRIMARY'),
-                        );
-                        interaction.editReply({ content: 'Il pezzente sta riproducendo', ephemeral: true, components: [row] });           
+                    
+                        res.on("end", function() {
+                            var body = Buffer.concat(chunks);
+                            console.log(body.toString());
+                            interaction.editReply({ content: 'test test', ephemeral: true });     
+                        });
+                    
                     });
-                })
-            }).catch(function(error) {
-                console.log(error);
-            }); 
+                    
+                    req.end()
+                }
+            }
         }
 
     }
