db40f1c90edb5dffa04397a0bc1d54bc54185ce7
blast <rodfval.fv@gmail.com>
2022-09-16 15:56:26 +0000

    aaa

---
+++ commands/autotimeout_cmd.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const fs = require('fs');
+const config = require("../config.json");
+const fetch = require('node-fetch');
+
+const path = config.CACHE_DIR;
+const api=config.API_URL;
+const text="&text=";
+const path_text=config.API_PATH_TEXT
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('autotimeout')
+        .setDescription("Abilita o Disabilita il bot ad ascoltare tutto e rispondere a 'tono'")
+        .addIntegerOption(option => option.setName('secs').setDescription('Tempo in secondi').setRequired(true)),
+    async execute(interaction) {
+        const secs = interaction.options.getInteger('secs');
+        config.AUTONOMOUS_TIMEOUT=(secs*1000);
+        interaction.reply({ content: "Timeout impostato per la modalità automatica: " + secs + " secondi", ephemeral: true });
+    }
+}; 
